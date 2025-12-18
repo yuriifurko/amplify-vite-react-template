f5dbfc15e139e9e2e7a71e83802ac2eb7ef8f393
yurii.furko <yurii.furko@gmail.com>
2025-12-18 23:18:38 +0200

    add storage backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,7 +2,10 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
+import { storage } from './storage/resource';
+
 defineBackend({
   auth,
+  storage,
   data,
 });
